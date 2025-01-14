4a2842b4f88a98c777033eb22518c83ee5dcd2c8
renantrendt <159676672+renantrendt@users.noreply.github.com>
2025-01-13 19:49:07 -0800

    a

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -53,27 +53,7 @@ function CategoryList({
           </button>
         </div>
       )}
-      <div className="mb-4">
-        {isAuthenticated && (
-          <button
-            onClick={() => setShowAddCategory(true)}
-            className="flex items-center justify-center w-full p-2 border-2 border-dashed border-gray-300 rounded-lg hover:border-gray-400 transition-colors"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 text-gray-400"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-        )}
-      </div>
+      
       <ul className="space-y-2">
         {categories.map((category) => (
           <li
