ef7caca521f09f6d06b6b4906b61a7e3b1af5b77
renantrendt <159676672+renantrendt@users.noreply.github.com>
2025-01-17 20:54:11 -0800

    ms

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../utils/supabase';
-import { FaPencilAlt, FaTrash } from 'react-icons/fa';
 
 function CategoryList({ 
   categories, 
@@ -138,18 +137,22 @@ function CategoryList({
                           e.stopPropagation();
                           startEditing(category);
                         }}
-                        className="text-blue-500 hover:text-blue-600"
+                        className="text-gray-400 hover:text-blue-600 transition-colors"
                       >
-                        <FaPencilAlt />
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                          <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                        </svg>
                       </button>
                       <button
                         onClick={(e) => {
                           e.stopPropagation();
                           handleDeleteCategory(category);
                         }}
-                        className="text-red-500 hover:text-red-600"
+                        className="text-gray-400 hover:text-red-600 transition-colors"
                       >
-                        <FaTrash />
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                        </svg>
                       </button>
                       <button
                         onClick={(e) => {
